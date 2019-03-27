8763aeb4b335281f9921ebb8edfbe6ad0802bc67
Orest-2 <orest-2@yandex.ua>
2019-03-27 13:31:03 +0200

    some improvements

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,23 @@
 class PagesController < ApplicationController
   def index
     @home = true
+    @rooms = [
+      {
+        id: 1,
+        title: 'For one person',
+        price: 50
+      },
+      {
+        id: 2,
+        title: 'For two person',
+        price: 90
+      },
+      {
+        id: 3,
+        title: 'For tree person',
+        price: 145
+      }
+    ]
   end
   def gallery
     @gallery = true
